75cc6d3077903e3275689bf7266f1320e695def6
Ahmad Luqman <ahmad-luqman@users.noreply.github.com>
2022-11-14 11:11:38 +0500

    add searchbar and styling

---
+++ src/components/SearchBar.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { TextInput, StyleSheet, View } from "react-native";
+import { Feather } from "@expo/vector-icons";
+
+const SearchBar = () => {
+  return (
+    <View style={styles.backgroundStyle}>
+      <Feather name="search" style={styles.iconStyle} />
+      <TextInput
+        style={styles.inputStyle}
+        placeholder="Search"
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  backgroundStyle: {
+    marginTop: 10,
+    backgroundColor: "#F0EEEE",
+    height: 50,
+    borderRadius: 5,
+    marginHorizontal: 15,
+    flexDirection: "row",
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: "center"
+  },
+  inputStyle: {
+    flex: 1,
+    fontSize: 18,
+  }
+});
+
+export default SearchBar;
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { Text, StyleSheet, View } from "react-native";
+import SearchBar from "../components/SearchBar";
 
 const SearchScreen = () => {
   return (
-    <View>
+    <View style={styles.background}>
+      <SearchBar/>
       <Text>Search Screen</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: "#FFFFFF",
+  }
+});
 
 export default SearchScreen;
