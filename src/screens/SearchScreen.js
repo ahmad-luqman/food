1c3ad46807ff9486b4e2dbd213a1ebf88768a3cb
Ahmad Luqman <ahmad-luqman@users.noreply.github.com>
2022-11-14 13:02:07 +0500

    add state and yelp api

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, StyleSheet, View } from "react-native";
 import SearchBar from "../components/SearchBar";
+import yelp from "../api/yelp";
 
 const SearchScreen = () => {
+  const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+
+  const searchApi = async () => {
+    const response = await yelp.get("/search", {
+      params: {
+        limit: 50,
+        term,
+        location: "san jose",
+      },
+    });
+    setResults(response.data.businesses);
+  };
+
   return (
-    <View style={styles.background}>
-      <SearchBar/>
+    <View>
+      <SearchBar
+        term={term}
+        onTermChange={newTerm => setTerm(newTerm)}
+        onTermSubmit={searchApi}
+      />
       <Text>Search Screen</Text>
+      <Text>We have found {results.length} results</Text>
     </View>
   );
 };
